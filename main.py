c2070d0e7785eb12e834245e8601e7348dad94d2
Jan Spacek <jan.spacek12@gmail.com>
2019-02-21 15:38:37 +0100

    finish up

--- main.py
+++ main.py
@@ -16,6 +16,12 @@ logging.basicConfig(level=logging.INFO)
 
 
 async def get_image(session, url):
+    """Get single image on the provided url
+
+    :param session: http session passed to the function
+    :param url: image url
+    :return: image in bytes format
+    """
     async with session.get(url) as resp:
         if resp.status != 200:
             logging.error(f'response status != 200 for image {url}')
@@ -24,6 +30,11 @@ async def get_image(session, url):
 
 
 async def get_page(session, url):
+    """Try to get content on the url
+    :param session: session passed to the function
+    :param url: page url
+    :return: page content returned if request successful. if error None is returned
+    """
     try:
         resp = await session.get(url)
         if resp.status != 200:
@@ -38,18 +49,30 @@ async def get_page(session, url):
         return None
 
 
-async def get_all_relevant_subpagese(session, url='https://exponea.com/'):
+async def get_all_relevant_subpages(session, main_page):
+    """Get all sub pages with the main page domain
 
+    :param session: session passed to the function
+    :param main_page: page used to search for sub pages
+    :return: list of relevant sub pages
+    """
+    url = f'https://{main_page}'
     content = await get_page(session, url)
 
     soup = BeautifulSoup(content, features="html.parser")
     links = [link.get('href') for link in soup.find_all('a', attrs={'href': re.compile("^http")})]
-    exponea_links = [link for link in links if 'exponea.com' in link]
+    relevant_links = [link for link in links if main_page in link]
 
-    return exponea_links
+    return relevant_links
 
 
-async def get_url_images(session, url='https://exponea.com/'):
+async def get_url_images(session, url):
+    """Get all images available on the provided url
+
+    :param session: http session passed to the function
+    :param url:
+    :return: list of tuples in format (source, bytes image)
+    """
     content = await get_page(session, url)
     if not content:
         return []
@@ -65,7 +88,11 @@ async def get_url_images(session, url='https://exponea.com/'):
     return images
 
 
-async def save_images(images):
+async def save_url_images(images):
+    """Save all provided images to the hard drive
+
+    :param images: list of image tuples from get_url_images
+    """
     for source, image in images:
         name = source.split('/')[-1]
         async with aiofiles.open(f'{OUTPUT_FOLDER}/{name}', 'wb') as f:
@@ -73,21 +100,41 @@ async def save_images(images):
 
 
 async def scrape_page(session, url):
+    """Get and save all the images found on the page
+
+    :param session: http session passed to the function
+    :param url: url of the page
+    """
     logging.info('Scraping %s', url)
     images = await get_url_images(session, url)
-    await save_images(images)
+    await save_url_images(images)
+
+
+async def scrape_pages(session, pages):
+        """Get and save all the images found on all the pages
+
+        :param session: http session passed to the function
+        :param pages: list of pages
+        """
+        tasks = [scrape_page(session, url) for url in pages]
+        await asyncio.gather(*tasks)
+
+
+async def download_all_images(main_page):
+    """Download all images found on the main page and its sub pages
+
+    :param main_page: main page used to search for sub pages and images
+    """
+    all_relevant_pages = [f'https://{main_page}']
+    async with aiohttp.ClientSession() as session:
+        subpages = await get_all_relevant_subpages(session, main_page)
+        all_relevant_pages.extend(subpages)
+
+        await scrape_pages(session, all_relevant_pages)
+
 
 async def main():
-
-    main_page = 'exponea.com'
-    main_url = f'https://{main_page}'
-
-    async with aiohttp.ClientSession() as session:
-
-        subpages = await get_all_relevant_subpagese(session, url=main_url)
-
-        tasks = [scrape_page(session, url) for url in subpages]
-        await asyncio.gather(*tasks)
+    await download_all_images('exponea.com')
 
 
 if __name__ == "__main__":
